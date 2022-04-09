f8b78e6d931705ed2fb459bc661171da64534279
bhavesh <bhavesh.daswani@toptal.com>
2022-04-09 16:35:59 +0530

    redux typescript with search

--- src/components/repositories-list.tsx
+++ src/components/repositories-list.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('')
+  const { searchRepository } = useActions()
+  const { data, error, loading } = useTypedSelector(state => state.repositoryReducer);
+
+  const onSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    searchRepository(term);
+  }
+
   return (
   <div>
-    <form>
-      <input />
-      <button>Search</button>
+    <form onSubmit={onSearch}>
+      <input value={term} onChange={(e) => setTerm(e.target.value)} />
+      <button type="submit">Search</button>
     </form>
+    {error && error}
+    {loading && <div>loading... </div> }
+    {!error && !loading && data && data.map(result => <p key={result}>{result}</p>) }
   </div>
   )
 }
